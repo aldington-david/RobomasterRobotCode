fa6c7c1ae624dbc746d8001f296a0c30644721d8
alding-david <ken_neth@live.cn>
2022-01-24 15:36:58 +0800

    referee tx temp changed 3
    Signed-off-by: alding-david <ken_neth@live.cn>

--- Core/Inc/tim.h
+++ Core/Inc/tim.h
@@ -44,9 +44,7 @@ extern TIM_HandleTypeDef htim10;
 /* USER CODE END Private defines */
 
 void MX_TIM1_Init(void);
-
 void MX_TIM2_Init(void);
-
 void MX_TIM3_Init(void);
 void MX_TIM4_Init(void);
 void MX_TIM5_Init(void);
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -93,16 +93,15 @@ void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
-  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
-  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
-  sBreakDeadTimeConfig.DeadTime = 0;
-  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
-  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+    if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_4) != HAL_OK) {
+        Error_Handler();
+    }
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+    sBreakDeadTimeConfig.DeadTime = 0;
+    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
     sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
     if (HAL_TIMEx_ConfigBreakDeadTime(&htim1, &sBreakDeadTimeConfig) != HAL_OK) {
         Error_Handler();
@@ -130,7 +129,7 @@ void MX_TIM2_Init(void) {
     htim2.Instance = TIM2;
     htim2.Init.Prescaler = 2799;
     htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim2.Init.Period = 1000;
+    htim2.Init.Period = 999;
     htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
     if (HAL_TIM_Base_Init(&htim2) != HAL_OK) {
@@ -158,11 +157,11 @@ void MX_TIM3_Init(void) {
 
     /* USER CODE END TIM3_Init 0 */
 
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+    TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+    TIM_MasterConfigTypeDef sMasterConfig = {0};
+    TIM_OC_InitTypeDef sConfigOC = {0};
 
-  /* USER CODE BEGIN TIM3_Init 1 */
+    /* USER CODE BEGIN TIM3_Init 1 */
 
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
@@ -436,77 +435,76 @@ void MX_TIM10_Init(void)
 
 }
 
-void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
-{
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle) {
 
-  if(tim_baseHandle->Instance==TIM1) {
-      /* USER CODE BEGIN TIM1_MspInit 0 */
+    if (tim_baseHandle->Instance == TIM1) {
+        /* USER CODE BEGIN TIM1_MspInit 0 */
 
-      /* USER CODE END TIM1_MspInit 0 */
-      /* TIM1 clock enable */
-      __HAL_RCC_TIM1_CLK_ENABLE();
-      /* USER CODE BEGIN TIM1_MspInit 1 */
+        /* USER CODE END TIM1_MspInit 0 */
+        /* TIM1 clock enable */
+        __HAL_RCC_TIM1_CLK_ENABLE();
+        /* USER CODE BEGIN TIM1_MspInit 1 */
 
-      /* USER CODE END TIM1_MspInit 1 */
-  } else if (tim_baseHandle->Instance == TIM2) {
-      /* USER CODE BEGIN TIM2_MspInit 0 */
+        /* USER CODE END TIM1_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM2) {
+        /* USER CODE BEGIN TIM2_MspInit 0 */
 
-      /* USER CODE END TIM2_MspInit 0 */
-      /* TIM2 clock enable */
-      __HAL_RCC_TIM2_CLK_ENABLE();
+        /* USER CODE END TIM2_MspInit 0 */
+        /* TIM2 clock enable */
+        __HAL_RCC_TIM2_CLK_ENABLE();
 
-      /* TIM2 interrupt Init */
-      HAL_NVIC_SetPriority(TIM2_IRQn, 5, 0);
-      HAL_NVIC_EnableIRQ(TIM2_IRQn);
-      /* USER CODE BEGIN TIM2_MspInit 1 */
+        /* TIM2 interrupt Init */
+        HAL_NVIC_SetPriority(TIM2_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(TIM2_IRQn);
+        /* USER CODE BEGIN TIM2_MspInit 1 */
 
-      /* USER CODE END TIM2_MspInit 1 */
-  } else if (tim_baseHandle->Instance == TIM3) {
-      /* USER CODE BEGIN TIM3_MspInit 0 */
+        /* USER CODE END TIM2_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM3) {
+        /* USER CODE BEGIN TIM3_MspInit 0 */
 
-      /* USER CODE END TIM3_MspInit 0 */
-      /* TIM3 clock enable */
-      __HAL_RCC_TIM3_CLK_ENABLE();
-      /* USER CODE BEGIN TIM3_MspInit 1 */
+        /* USER CODE END TIM3_MspInit 0 */
+        /* TIM3 clock enable */
+        __HAL_RCC_TIM3_CLK_ENABLE();
+        /* USER CODE BEGIN TIM3_MspInit 1 */
 
-      /* USER CODE END TIM3_MspInit 1 */
-  } else if(tim_baseHandle->Instance == TIM4) {
-      /* USER CODE BEGIN TIM4_MspInit 0 */
+        /* USER CODE END TIM3_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM4) {
+        /* USER CODE BEGIN TIM4_MspInit 0 */
 
-      /* USER CODE END TIM4_MspInit 0 */
-      /* TIM4 clock enable */
-      __HAL_RCC_TIM4_CLK_ENABLE();
-      /* USER CODE BEGIN TIM4_MspInit 1 */
+        /* USER CODE END TIM4_MspInit 0 */
+        /* TIM4 clock enable */
+        __HAL_RCC_TIM4_CLK_ENABLE();
+        /* USER CODE BEGIN TIM4_MspInit 1 */
 
-      /* USER CODE END TIM4_MspInit 1 */
-  } else if(tim_baseHandle->Instance==TIM5) {
-      /* USER CODE BEGIN TIM5_MspInit 0 */
+        /* USER CODE END TIM4_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM5) {
+        /* USER CODE BEGIN TIM5_MspInit 0 */
 
-      /* USER CODE END TIM5_MspInit 0 */
-      /* TIM5 clock enable */
-      __HAL_RCC_TIM5_CLK_ENABLE();
-      /* USER CODE BEGIN TIM5_MspInit 1 */
+        /* USER CODE END TIM5_MspInit 0 */
+        /* TIM5 clock enable */
+        __HAL_RCC_TIM5_CLK_ENABLE();
+        /* USER CODE BEGIN TIM5_MspInit 1 */
 
-      /* USER CODE END TIM5_MspInit 1 */
-  } else if(tim_baseHandle->Instance==TIM8) {
-      /* USER CODE BEGIN TIM8_MspInit 0 */
+        /* USER CODE END TIM5_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM8) {
+        /* USER CODE BEGIN TIM8_MspInit 0 */
 
-      /* USER CODE END TIM8_MspInit 0 */
-      /* TIM8 clock enable */
-      __HAL_RCC_TIM8_CLK_ENABLE();
-      /* USER CODE BEGIN TIM8_MspInit 1 */
+        /* USER CODE END TIM8_MspInit 0 */
+        /* TIM8 clock enable */
+        __HAL_RCC_TIM8_CLK_ENABLE();
+        /* USER CODE BEGIN TIM8_MspInit 1 */
 
-      /* USER CODE END TIM8_MspInit 1 */
-  } else if(tim_baseHandle->Instance==TIM10) {
-      /* USER CODE BEGIN TIM10_MspInit 0 */
+        /* USER CODE END TIM8_MspInit 1 */
+    } else if (tim_baseHandle->Instance == TIM10) {
+        /* USER CODE BEGIN TIM10_MspInit 0 */
 
-      /* USER CODE END TIM10_MspInit 0 */
-      /* TIM10 clock enable */
-      __HAL_RCC_TIM10_CLK_ENABLE();
-      /* USER CODE BEGIN TIM10_MspInit 1 */
+        /* USER CODE END TIM10_MspInit 0 */
+        /* TIM10 clock enable */
+        __HAL_RCC_TIM10_CLK_ENABLE();
+        /* USER CODE BEGIN TIM10_MspInit 1 */
 
-      /* USER CODE END TIM10_MspInit 1 */
-  }
+        /* USER CODE END TIM10_MspInit 1 */
+    }
 }
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
 {
@@ -666,76 +664,75 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
 
 }
 
-void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
-{
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle) {
 
-  if(tim_baseHandle->Instance==TIM1) {
-      /* USER CODE BEGIN TIM1_MspDeInit 0 */
+    if (tim_baseHandle->Instance == TIM1) {
+        /* USER CODE BEGIN TIM1_MspDeInit 0 */
 
-      /* USER CODE END TIM1_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM1_CLK_DISABLE();
-      /* USER CODE BEGIN TIM1_MspDeInit 1 */
+        /* USER CODE END TIM1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM1_CLK_DISABLE();
+        /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
-      /* USER CODE END TIM1_MspDeInit 1 */
-  } else if (tim_baseHandle->Instance == TIM2) {
-      /* USER CODE BEGIN TIM2_MspDeInit 0 */
+        /* USER CODE END TIM1_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM2) {
+        /* USER CODE BEGIN TIM2_MspDeInit 0 */
 
-      /* USER CODE END TIM2_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM2_CLK_DISABLE();
+        /* USER CODE END TIM2_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM2_CLK_DISABLE();
 
-      /* TIM2 interrupt Deinit */
-      HAL_NVIC_DisableIRQ(TIM2_IRQn);
-      /* USER CODE BEGIN TIM2_MspDeInit 1 */
+        /* TIM2 interrupt Deinit */
+        HAL_NVIC_DisableIRQ(TIM2_IRQn);
+        /* USER CODE BEGIN TIM2_MspDeInit 1 */
 
-      /* USER CODE END TIM2_MspDeInit 1 */
-  } else if (tim_baseHandle->Instance == TIM3) {
-      /* USER CODE BEGIN TIM3_MspDeInit 0 */
+        /* USER CODE END TIM2_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM3) {
+        /* USER CODE BEGIN TIM3_MspDeInit 0 */
 
-      /* USER CODE END TIM3_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM3_CLK_DISABLE();
-      /* USER CODE BEGIN TIM3_MspDeInit 1 */
+        /* USER CODE END TIM3_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM3_CLK_DISABLE();
+        /* USER CODE BEGIN TIM3_MspDeInit 1 */
 
-      /* USER CODE END TIM3_MspDeInit 1 */
-  } else if(tim_baseHandle->Instance == TIM4) {
-      /* USER CODE BEGIN TIM4_MspDeInit 0 */
+        /* USER CODE END TIM3_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM4) {
+        /* USER CODE BEGIN TIM4_MspDeInit 0 */
 
-      /* USER CODE END TIM4_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM4_CLK_DISABLE();
-      /* USER CODE BEGIN TIM4_MspDeInit 1 */
+        /* USER CODE END TIM4_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM4_CLK_DISABLE();
+        /* USER CODE BEGIN TIM4_MspDeInit 1 */
 
-      /* USER CODE END TIM4_MspDeInit 1 */
-  } else if(tim_baseHandle->Instance==TIM5) {
-      /* USER CODE BEGIN TIM5_MspDeInit 0 */
+        /* USER CODE END TIM4_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM5) {
+        /* USER CODE BEGIN TIM5_MspDeInit 0 */
 
-      /* USER CODE END TIM5_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM5_CLK_DISABLE();
-      /* USER CODE BEGIN TIM5_MspDeInit 1 */
+        /* USER CODE END TIM5_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM5_CLK_DISABLE();
+        /* USER CODE BEGIN TIM5_MspDeInit 1 */
 
-      /* USER CODE END TIM5_MspDeInit 1 */
-  } else if(tim_baseHandle->Instance==TIM8) {
-      /* USER CODE BEGIN TIM8_MspDeInit 0 */
+        /* USER CODE END TIM5_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM8) {
+        /* USER CODE BEGIN TIM8_MspDeInit 0 */
 
-      /* USER CODE END TIM8_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM8_CLK_DISABLE();
-      /* USER CODE BEGIN TIM8_MspDeInit 1 */
+        /* USER CODE END TIM8_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM8_CLK_DISABLE();
+        /* USER CODE BEGIN TIM8_MspDeInit 1 */
 
-      /* USER CODE END TIM8_MspDeInit 1 */
-  } else if(tim_baseHandle->Instance==TIM10) {
-      /* USER CODE BEGIN TIM10_MspDeInit 0 */
+        /* USER CODE END TIM8_MspDeInit 1 */
+    } else if (tim_baseHandle->Instance == TIM10) {
+        /* USER CODE BEGIN TIM10_MspDeInit 0 */
 
-      /* USER CODE END TIM10_MspDeInit 0 */
-      /* Peripheral clock disable */
-      __HAL_RCC_TIM10_CLK_DISABLE();
-      /* USER CODE BEGIN TIM10_MspDeInit 1 */
+        /* USER CODE END TIM10_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM10_CLK_DISABLE();
+        /* USER CODE BEGIN TIM10_MspDeInit 1 */
 
-      /* USER CODE END TIM10_MspDeInit 1 */
-  }
+        /* USER CODE END TIM10_MspDeInit 1 */
+    }
 }
 
 /* USER CODE BEGIN 1 */
