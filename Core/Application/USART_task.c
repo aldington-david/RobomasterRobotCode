8684366924f2dbc7a3c24661902c880e41237144
alding-david <ken_neth@live.cn>
2022-04-16 18:47:57 +0800

    rename usart_task

---
+++ Core/Application/USART_task.c
@@ -0,0 +1,9 @@
+//
+// Created by Ken_n on 2022/4/16.
+//
+
+#include "USART_task.h"
+
+void USART_task(void const *pvParameters) {
+
+}
---
+++ Core/Application/USART_task.h
@@ -0,0 +1,16 @@
+//
+// Created by Ken_n on 2022/4/16.
+//
+
+#ifndef ROBOMASTERROBOTCODE_USART_TASK_H
+#define ROBOMASTERROBOTCODE_USART_TASK_H
+
+#endif //ROBOMASTERROBOTCODE_USART_TASK_H
+
+
+/**
+  * @brief          串口发送任务，由发送时任务通知激活
+  * @param[in]      pvParameters: 空
+  * @retval         none
+  */
+void USART_task(void const *pvParameters);
