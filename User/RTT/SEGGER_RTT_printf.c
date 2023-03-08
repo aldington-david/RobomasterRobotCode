f77af69479b456d23015294259e367497039c9f4
alding-david <ken_neth@live.cn>
2023-03-08 16:38:19 +0800

    update SEGGER_RTT

--- User/RTT/SEGGER_RTT_printf.c
+++ User/RTT/SEGGER_RTT_printf.c
@@ -3,7 +3,7 @@
 *                        The Embedded Experts                        *
 **********************************************************************
 *                                                                    *
-*            (c) 1995 - 2019 SEGGER Microcontroller GmbH             *
+*            (c) 1995 - 2021 SEGGER Microcontroller GmbH             *
 *                                                                    *
 *       www.segger.com     Support: support@segger.com               *
 *                                                                    *
@@ -42,7 +42,7 @@
 *                                                                    *
 **********************************************************************
 *                                                                    *
-*       RTT version: 6.88c                                           *
+*       RTT version: 7.86b                                           *
 *                                                                    *
 **********************************************************************
 
@@ -478,7 +478,7 @@ int SEGGER_RTT_vprintf(unsigned BufferIndex, const char *sFormat, va_list *pPara
                     _PrintInt(&BufferDesc, v, 10u, 4, FieldWidth + 1, FormatFlags);          //显示小数点后两位
                 }
                     break;
-//---------------------------------------
+                    //---------------------------------------
                 default:
                     break;
             }
